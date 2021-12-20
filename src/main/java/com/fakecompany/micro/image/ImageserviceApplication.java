c1ab5ae1e8c33bd32c8816caafb37912bf6213b8
antipatron <juanguillermoalarcon@gmail.com>
2021-12-20 12:51:08 -0500

    First commit

---
+++ src/main/java/com/fakecompany/micro/image/ImageserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fakecompany.micro.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageserviceApplication.class, args);
+	}
+
+}
